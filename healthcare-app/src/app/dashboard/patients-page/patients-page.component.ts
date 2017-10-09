c470f283404ae7b18101aaa9859f6dfac9f47d98
OstapHetman <prickly4528@gmail.com>
2017-10-09 14:37:35 +0300

    add html database table

--- healthcare-app/src/app/dashboard/patients-page/patients-page.component.ts
+++ healthcare-app/src/app/dashboard/patients-page/patients-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'patients-page',
@@ -10,6 +11,7 @@ export class PatientsPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.collapsible').collapsible();
   }
 
 }
